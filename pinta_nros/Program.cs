c22e372bb413bacda3a61155f54665d29582a7c1
CastilloGit <jcarlos.castillov@gmail.com>
2020-10-10 01:16:14 +0200

    Add files via upload

---
+++ pinta_nros/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace pinta_nros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = 7;
+            string asterisk = "*";
+            string espacio = "";
+
+            Console.WriteLine("Letra R: ");
+            for(int row=0; row < lenght; row++)
+            {
+                for(int column = 0; column < lenght; column++)
+                {
+                      Console.Write(
+                        (column == 0) ||
+                        ((row == 0 || row == 4) && column < 4) ||
+                        (column == 4 && (row == 0 || row == 3 || row == 5)) ||
+                        (column == 5 && (row == 1 || row == 2 || row == 6))
+                    ? asterisk : " ");
+
+                }
+                Console.WriteLine(espacio);
+            }
+
+            Console.WriteLine("Letra I: ");
+            for(int fila = 0; fila < lenght; fila++)
+            {
+                for(int col=0; col < lenght; col++)
+                {
+                      Console.Write(
+                        ((fila == 0 ) && (col > 0 && col < 6)) ||
+                        (col == 3) ||
+                        ((fila == 6 ) && (col > 0 && col < 6))
+                    ? asterisk : " ");
+
+                }
+                Console.WriteLine(espacio);
+            }            
+        }
+    }
+}
